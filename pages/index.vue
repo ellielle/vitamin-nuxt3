5908acc5b12cf0d3cf1e49546c3bbe4dee22c1eb
ellielle <40385743+ellielle@users.noreply.github.com>
2023-01-08 16:28:07 -0500

    add alternate basic index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,18 @@
+<script setup lang="ts">
+  const clickHandler = async () => {
+    // Server and Browser should show different logs
+    const response = await $fetch("/api/intro");
+    console.log(response);
+  };
+</script>
+
 <template>
-  <NuxtWelcome />
+  <!-- <NuxtWelcome /> -->
+  <div class="flex place-content-center pt-24">
+    <div class="i-logos-vue text-8xl"></div>
+    <div class="i-logos-nuxt text-8xl"></div>
+  </div>
+  <div class="flex flex-col">
+    <button class="justify-center mt-12" @click="clickHandler">CSS is reset. This is a button.</button>
+  </div>
 </template>
