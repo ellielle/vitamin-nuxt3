ec19cff08b38c74ef1b4bb6891f6286391b844f4
ellielle <40385743+ellielle@users.noreply.github.com>
2023-01-07 19:28:07 -0500

    get testing to work, add simple example

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,5 @@ export default defineNuxtConfig({
   experimental: {
     reactivityTransform: true,
   },
-  css: [
-    "@unocss/reset/tailwind.css"
-  ]
+  css: ["@unocss/reset/tailwind.css"],
 });
--- tests/intro.test.ts
+++ tests/intro.test.ts
@@ -1,8 +1,17 @@
-import { $fetch, setup } from "@nuxt/test-utils";
+import { fileURLToPath } from "node:url";
+import { $fetch, fetch, setup } from "@nuxt/test-utils";
 import { describe, expect, it } from "vitest";
 
 describe("index.vue", async () => {
-  it("it gets the intro from the server", async () => {
-    
+  await setup({
+    rootDir: fileURLToPath(new URL("..", import.meta.url)),
+    server: true,
+  });
+
+  it("it gets the intro from the api", async () => {
+    const introData = await $fetch("/api/intro");
+    console.log("introdata ", introData);
+
+    expect(introData.intro).toContain("Welcome to Nuxt 3");
   });
 });
