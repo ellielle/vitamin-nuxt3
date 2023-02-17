fbd6777f3378f77e1e9963c7e9d1345229498f71
ellielle <40385743+ellielle@users.noreply.github.com>
2023-02-17 11:48:15 -0500

    update configs and deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@vueuse/nuxt", "@unocss/nuxt"],
-  experimental: {
-    reactivityTransform: true,
+  typescript: {
+    shim: false,
   },
-  css: ["@unocss/reset/tailwind.css"],
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      title: "Wow Nuxt 3!",
+      meta: [{ name: "description", content: "Nuxt 3 is pretty neat." }],
+    },
+  },
 });
